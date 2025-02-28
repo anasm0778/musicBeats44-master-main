534b4eb70cf37b306ee730e3ddecdb37f677329f
anasm0778 <anasm0778@gmail.com>
2025-02-28 12:28:12 +0530

    change

--- test_spotify_api.py
+++ test_spotify_api.py
@@ -2,7 +2,7 @@ import spotipy
 import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='1ff69d94deb24751b4e1a55c9bf9301d',client_secret='05f6def4895641848da6cfeaa6afceef'))
+# spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='1ff69d94deb24751b4e1a55c9bf9301d',client_secret='05f6def4895641848da6cfeaa6afceef'))
 
 if len(sys.argv) > 1:
     name = ' '.join(sys.argv[1:])
